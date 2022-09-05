9e7d74224032cfc5fda6cc92110be666edc1f575
YongHo Song <spphire2@gmail.com>
2022-09-05 15:19:11 +0900

    추가 세팅

--- src/main/java/com/ll/exam/app10/app/home/controller/App10Application.java
+++ src/main/java/com/ll/exam/app10/app/home/controller/App10Application.java
@@ -1,9 +1,11 @@
-package com.ll.exam.app10;
+package com.ll.exam.app10.app.home.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class App10Application {
 
 	public static void main(String[] args) {

c5e09bc24b2b5a1179688ac15530970ea98a3363
YongHo Song <spphire2@gmail.com>
2022-09-06 15:51:15 +0900

    회원가입 프로필 수정
    반영

---
+++ src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
@@ -0,0 +1,49 @@
+package com.ll.exam.app10.app.member.controller;
+
+import com.ll.exam.app10.app.member.entity.Member;
+import com.ll.exam.app10.app.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import javax.servlet.http.HttpSession;
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+    @GetMapping("/join")
+    public String showJoin() {
+        return "member/join";
+    }
+    @PostMapping("/join")
+    public String join(String username, String password, String email, MultipartFile profileImg, HttpSession session) {
+        Member oldMember = memberService.getMemberByUsername(username);
+        if (oldMember != null) {
+            return "redirect:/?errorMsg=Already done.";
+        }
+        Member member = memberService.join(username, "{noop}" + password, email, profileImg);
+        session.setAttribute("loginedMemberId", member.getId());
+        return "redirect:/member/profile";
+    }
+
+    @GetMapping("/profile")
+    public String showProfile(HttpSession session, Model model) {
+        Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
+        boolean isLogined = loginedMemberId != null;
+
+        if (isLogined == false) {
+            return "redirect:/?errorMsg=Need to login!";
+        }
+
+        Member loginedMember = memberService.getMemberById(loginedMemberId);
+
+        model.addAttribute("loginedMember", loginedMember);
+
+        return "member/profile";
+    }
+}

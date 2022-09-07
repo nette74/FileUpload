d555a4871f8033fa616a27648f0ab8eceda040c8
YongHo Song <spphire2@gmail.com>
2022-09-07 14:38:00 +0900

    스프링 시큐리티

--- src/main/java/com/ll/exam/app10/app/base/SecurityConfig.java
+++ src/main/java/com/ll/exam/app10/app/base/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +21,9 @@ public class SecurityConfig {
                 .antMatchers("/**")
                 .permitAll();
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
